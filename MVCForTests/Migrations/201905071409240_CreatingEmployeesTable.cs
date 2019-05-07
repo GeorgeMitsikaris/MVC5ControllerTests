6a5084a165d2051043de5e4d6fd3141dff607a09
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-05-07 17:18:53 +0300

    Creating EmployeesTable

---
+++ MVCForTests/Migrations/201905071409240_CreatingEmployeesTable.cs
@@ -0,0 +1,29 @@
+namespace MVCForTests.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatingEmployeesTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Gender = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
