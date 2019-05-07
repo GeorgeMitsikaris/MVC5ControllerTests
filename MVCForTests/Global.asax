4094b0bfa1338a087942764ece0e72a064b9128c
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-05-07 16:15:27 +0300

    Add project files.

---
+++ MVCForTests/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCForTests.MvcApplication" Language="C#" %>
---
+++ MVCForTests/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCForTests.Startup))]
+namespace MVCForTests
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
