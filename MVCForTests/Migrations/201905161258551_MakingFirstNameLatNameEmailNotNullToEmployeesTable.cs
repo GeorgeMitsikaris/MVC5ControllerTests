b461b4d8ad8ebb1b28c6cb48c66a11061984cd1f
George Mitsikaris <30596553+GeorgeMits@users.noreply.github.com>
2019-05-16 16:07:44 +0300

    Adding Required attribute to Firstname,Lastname and Email properties of Employee and updating the DB

---
+++ MVCForTests/Migrations/201905161258551_MakingFirstNameLatNameEmailNotNullToEmployeesTable.cs
@@ -0,0 +1,22 @@
+namespace MVCForTests.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MakingFirstNameLatNameEmailNotNullToEmployeesTable : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Employees", "FirstName", c => c.String(nullable: false));
+            AlterColumn("dbo.Employees", "LastName", c => c.String(nullable: false));
+            AlterColumn("dbo.Employees", "Email", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Employees", "Email", c => c.String());
+            AlterColumn("dbo.Employees", "LastName", c => c.String());
+            AlterColumn("dbo.Employees", "FirstName", c => c.String());
+        }
+    }
+}
--- MVCForTests/Models/Employee.cs
+++ MVCForTests/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,12 @@ namespace MVCForTests.Models
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
         public string Gender { get; set; }
+        [Required]
         public string Email { get; set; }
     }
 }
